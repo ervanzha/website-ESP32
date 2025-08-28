40a8264e3d76b59360a7463e4f97530f92600cbd
ervanzha <91467311+ervanzha@users.noreply.github.com>
2025-08-28 10:49:00 +0700

    post_ldr.php

--- sensor/post_ldr.php
+++ sensor/post_ldr.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$dbname = "u683623856_esp_data";
-$username = "u683623856_esp_user";
-$password = "Esp_pass321";
+$dbname = "esp_data";
+$username = "esp_user";
+$password = "Esp_pass";
 
 $ldr = $_POST['ldr'];
 
